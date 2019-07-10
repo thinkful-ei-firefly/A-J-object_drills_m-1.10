935c623328834a13de1460a11847abe5b17b3b5d
Ahmed <ahmed.max7586@gmail.com>
2019-07-10 11:52:15 -0400

    modifyed file loc and added fn self_referenc

---
+++ drill_1/Self-reference.js
@@ -0,0 +1,10 @@
+function personMaker() {
+    var person = {
+        firstName: 'Paul',
+        lastName: 'Jones',
+        fullName: function () {
+            return `${this.firstName} ${this.lastName}`
+        },
+    };
+    return person;
+}
