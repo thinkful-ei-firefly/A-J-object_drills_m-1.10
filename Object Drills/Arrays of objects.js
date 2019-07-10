455b34301df48f2fbccb19351d3026b06c3ecfda
Julianb12 <julianb1212@gmail.com>
2019-07-10 14:20:56 -0500

    added Arrays of objects drill

---
+++ Object Drills/Arrays of objects.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let array = [
+
+    {name : 'John', jobTitle : 'Programmer'},
+    {name : 'Tom', jobTitle : 'Engineer'},
+    {name : 'Dan', jobTitle : 'Doctor'},
+    {name : 'Angela', jobTitle : 'Manager'}
+
+]
+
+for (let i = 0; i < array.length; i++){
+    console.log(array[i].jobTitle, array[i].name)
+
+}
