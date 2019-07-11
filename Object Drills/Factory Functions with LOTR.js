b0c5ba1eec3971c0fb6638dab87c47bc8129a469
Ahmed <ahmed.max7586@gmail.com>
2019-07-11 06:56:49 -0400

    added bonus drill

--- Object Drills/Factory Functions with LOTR.js
+++ Object Drills/Factory Functions with LOTR.js
@@ -1,4 +1,4 @@
-function createCharacter(name, nickname, race, origin, attack, defense) {
+function createCharacter(name, nickname, race, origin, attack, defense, weapon) {
     return {
         name,
         nickname,
@@ -6,13 +6,14 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
         origin,
         attack,
         defense,
-        describe () {
-            return `${this.name} is a ${this.race} from ${this.origin}.`;
+        weapon,
+        describe: function () {
+            return `${this.name} is a ${this.race} of the ${this.origin} who uses a ${this.weapon}`;
+
         },
         evaluateFight (character) {
 
-
-            return `Your opponent takes ${this.attack > character.defense ? this.attack > character.defense : 0} and you receive ${this.defense > character.attack ? this.defense - character.attack : 0}`;
+            return `Your opponent takes ${ character.defense > this.attack ? 0 :  this.attack -  character.defense } damage and you receive ${this.defense > character.attack ? 0 : character.attack - this.defense} damage`;
         }
     };
 }
@@ -23,6 +24,8 @@ const characters = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'M
     createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8, 'anduril'),
     createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5, 'Bow and Arrow')
 ];
+
+// console.log(characters[0].describe());
 
 characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 8, 7,'Hadhafang'));
 
@@ -46,4 +49,8 @@ const powerfullchar = characters.filter((char) => {
     }
 });
 
- const charWithWeapon = createCharacter()
+
+
+
+console.log(characters[0].describe());
+console.log(characters[1].evaluateFight(characters[0])); 
